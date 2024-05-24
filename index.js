93c575b91a0d4b2c78bc95c3f7967f54ad2c25f4
shaishabcoding <myshaishab@gmail.com>
2024-05-24 19:25:25 +0600

    get all user route

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ async function run() {
     const userCollection = client.db("bistroDb").collection("users");
 
     //users related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
