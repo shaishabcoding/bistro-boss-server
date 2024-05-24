7e214b1e0a96bf31a3c4dd47d62ca620a71503a4
shaishabcoding <myshaishab@gmail.com>
2024-05-24 18:45:11 +0600

    added users api

--- index.js
+++ index.js
@@ -28,6 +28,19 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const userCollection = client.db("bistroDb").collection("users");
+
+    //users related api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: true });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
